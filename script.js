80909c7e42b82861dca08cdef74a3bbb262ecb82
zbz31 <158506057+zbz31@users.noreply.github.com>
2024-02-27 19:25:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9228,7 +9228,7 @@ document.addEventListener("DOMContentLoaded", function () {
       break; // Exit the loop if maximum iterations reached
     }
 
-  } while (Math.min(...totality) < 3 && iterations < 100);
+  } while (Math.min(...totality) < 3);
   
 
   // var puzzle1 = [{ prow: ["es FC Barcelona", "es Atletico Madrid", "eng Man Utd"], pcolumn: ["de Bayern Munich", "Played WC 2022", "Played WC 2018"] },];
